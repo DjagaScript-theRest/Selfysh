14095dbfde5f0dfa092ee7f83cae0f995e7ced32
sguzunov <sguzunov@gmail.com>
2016-12-28 14:39:17 +0200

    Removed useless code from server.

--- utils/encryption.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const crypto = require('crypto');
-
-module.exports = {
-    generateSalt: () => crypto.randomBytes(128).toString('base64'),
-    generateHashedPassword: (salt, password) => crypto.createHmac('sha256', salt).update(password).digest('hex')
-};
